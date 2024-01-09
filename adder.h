32065477df6318693368c336894713b61eabbf96
callder <150067121+callder@users.noreply.github.com>
2024-01-09 11:43:09 +0100

    Add files via upload

--- adder.h
+++ adder.h
@@ -1,6 +1,15 @@
 #include <iostream>;
+#include <cmath>
 
 using namespace std;
+template <class T>
+bool maxHepify(T first, T second){
+    return first > second;
+}
+template <class T>
+bool minHepify(T first, T second){
+    return !(first > second);
+}
 
 void countingSort(int array[], int n){
     int max = array[0];
@@ -24,6 +33,7 @@ void countingSort(int array[], int n){
 
     delete[] count;
 }
+
 
 class MaxHeap {
 private:
@@ -122,5 +132,95 @@ public:
         heapDown(0);
         return root;
     }
+};
+
+template <class T>
+class DynamicArray{
+private:
+    int size;
+    T * array;
+    float increase = 2.0;
+public:
+    int lenght;
+
+    DynamicArray(){
+        size = 10;
+        array = new T[10];
+        lenght = 0;
+    }
+    ~DynamicArray(){
+        delete array;
+    }
+    bool sizeControl(){
+        return size*0.9 < lenght;
+    }
+    void sizeIncrease(){
+        size = size*increase;
+        auto *newArray = new T[size];
+        for (int i = 0; i < lenght; ++i) {
+            newArray[i] = array[i];
+        }
+        delete [] array;
+        array = newArray;
+    }
+    void add(T s){
+        if (sizeControl())
+            sizeIncrease();
+        array[lenght] = s;
+        lenght++;
+    }
+    void erase(){
+        delete [] array;
+        this->size = 10;
+        this->lenght = 0;
+    }
+    void modify(int ind, T s){
+        if (lenght<=ind){
+            cout << "Nieprawidlowy index" << endl;
+            return;
+        }
+        array[ind] = s;
+    }
+    void bubbleSort(bool(*cmp)(T, T)){
+        for(int i = 0; i<lenght;i++){
+            for(int j = 0; j<lenght-i-1; j++){
+                if(cmp(array[j],array[j+1])){
+                    int temp = array[j];
+                    array[j] =  array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+    void return_min(){
+        return array[0];
+    }
+    void return_max(){
+        return array[lenght];
+    }
+    T  * index(int index){
+        if(lenght<= index){
+            //  cout<<"Index out of range!";
+            return nullptr;
+        }
+        return &array[index];
+    }
+
+    void info(){
+        cout << "Size" << this->size << "Length" << this->lenght << endl;
+    }
+    void search(int ind){
+        if(lenght<= ind){
+            cout<<"Index out of range!";
+            return;
+        }
+        cout << array[ind] << endl;
+    }
+    void show(){
+        for (int i = 0; i < lenght; ++i) {
+            cout << array[i] << endl;
+        }
+    }
+
 };
 
